5571c7133757691f543515c37d7957735b625375
Stanislav <159009511590s@gmail.com>
2022-10-06 09:27:40 +0300

    It's my life!

---
+++ HelloVistulaPerekupenko/src/pl/vistula/helloworldperekupenko/HelloWorldPerekupenko.java
@@ -0,0 +1,11 @@
+package pl.vistula.helloworldperekupenko;
+
+public class HelloWorldPerekupenko {
+    public static void main(String[] args){
+        System.out.println("Hello Vistula Perekupenko");
+        System.out.println("It's my life");
+        System.out.println("It's now or never");
+        System.out.println("But I ain't gonna live forever");
+    }
+
+}
